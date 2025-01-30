2fc3b71d7dff0b88b983c54a2818a9db5d3d0f22
thomas <deponte.thomas@gmail.com>
2025-01-30 19:11:36 +0100

    Add Mark class and corresponding tests

---
+++ app/src/main/java/org/example/Mark.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Mark {
+    private Position position;
+    private Type type;
+
+    public Mark(Position position, Type type) {
+        this.position = position;
+        this.type = type;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
