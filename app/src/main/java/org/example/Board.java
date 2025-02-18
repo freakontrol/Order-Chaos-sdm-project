c662ca324d835c8cba672dfb14ade7aa36a5aaba
thomas <deponte.thomas@gmail.com>
2025-02-18 23:05:05 +0100

    refactor board

--- app/src/main/java/org/example/Board.java
+++ app/src/main/java/org/example/Board.java
@@ -5,85 +5,121 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Board {
-    private char[][] grid;
     private List<Move> moves;
     private HashMap<Position, Move> positionMoves;
+
+    class movesInRow {
+        private List<Move> upDiagMoves;
+        private List<Move> DownDiagMoves;
+        private List<Move> horizMoves;
+        private List<Move> vertMoves;
+
+        public movesInRow() {
+            upDiagMoves = new ArrayList<>();
+            DownDiagMoves = new ArrayList<>();
+            horizMoves = new ArrayList<>();
+            vertMoves = new ArrayList<>();
+        }
+
+        public void addUpDiagMove(Move move) {
+            upDiagMoves.add(move);
+        }
+        public void addDownDiagMove(Move move) {
+            DownDiagMoves.add(move);
+        }
+        public void addHorizMove(Move move) {
+            horizMoves.add(move);
+        }
+        public void addVertMove(Move move) {
+            vertMoves.add(move);
+        }
+        public void shareUpDiagMoves(movesInRow other) {
+            if (other != null) {
+                other.upDiagMoves = this.upDiagMoves;
+            }
+        }
+        public void shareDownDiagMoves(movesInRow other) {
+            if (other != null) {
+                other.DownDiagMoves = this.DownDiagMoves;
+            }
+        }
+        public void shareHorizMoves(movesInRow other) {
+            if (other != null) {
+                other.horizMoves = this.horizMoves;
+            }
+        }
+        public void shareVertMoves(movesInRow other) {
+            if (other != null) {
+                other.vertMoves = this.vertMoves;
+            }
+        }
+    }
+
+    private HashMap<Position, movesInRow> adjMoves;
 
 
     public Board() {
         this.moves = new ArrayList<>();
         this.positionMoves = new HashMap<>();
-
-        this.grid = new char[6][6];
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                grid[i][j] = ' '; // empty board
-            }
-        }
     }
 
     public void addMove(Move move) {
         moves.add(move);
         Position position = move.getMark().getPosition(); // TODO: use getter
-        if (!positionMoves.containsKey(position)) {
+        if (!positionMoves.containsKey(position) && !adjMoves.containsKey(position)) {
             positionMoves.put(position, move);
+
+            // check adjancent moves
+            movesInRow adjMoveList = new movesInRow();
+
+            Position adjPosUpLeft;
+            Position adjPosDownRight;
+
+            boolean isUpLeft = false;
+            boolean isDownRight = false;
+
+           
+            if(!position.isOnLeftEdge() || !position.isOnTopEdge()) {
+                adjPosUpLeft = position.getUpLeft();
+                if (positionMoves.containsKey(adjPosUpLeft)) {
+                    if (positionMoves.get(adjPosUpLeft).isMarkTypeEqual(move)) {
+                        adjMoves.get(adjPosUpLeft).addDownDiagMove(move);
+                        adjMoveList.shareDownDiagMoves(adjMoves.get(adjPosUpLeft));
+                        isUpLeft = true;
+                    }
+                }
+            }
+            if(!position.isOnRightEdge() || !position.isOnBottomEdge()) {
+                adjPosDownRight = position.getDownRight();
+                if (positionMoves.containsKey(adjPosDownRight)) {
+                    if (positionMoves.get(adjPosDownRight).isMarkTypeEqual(move)) {
+                        adjMoves.get(adjPosDownRight).addDownDiagMove(move);
+                        adjMoveList.shareDownDiagMoves(adjMoves.get(adjPosDownRight));
+                        isDownRight = true;
+                    }
+                }
+            }
+
+            if (isUpLeft && isDownRight) {
+                
+            }
+
+ 
+        } else {
+            throw new IllegalArgumentException("Multiple moves to same position are not allowed");
         }
 
-        char symbol = move.getMark().getType().getName().charAt(0);
-        grid[position.getRow()][position.getColumn()] = symbol;
-    }
-
-    public List<Move> getMoves() {
-        return new ArrayList<>(moves);
     }
 
     public void clearBoard() {
         moves.clear();
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                grid[i][j] = ' ';
-            }
-        }
+        positionMoves.clear();
     }
 
     public boolean isOccupied(Position position) {
-        return grid[position.getRow()][position.getColumn()] != ' ';
+        return positionMoves.containsKey(position);
     }
 
-    public boolean hasFiveInARow(int row, int col, char symbol) {
-        return checkDirection(row, col, symbol, 1, 0) || // horizontal
-               checkDirection(row, col, symbol, 0, 1) || // vertical
-               checkDirection(row, col, symbol, 1, 1) || // diagonal /
-               checkDirection(row, col, symbol, 1, -1);  // diagonal \
-    }
-
-    private boolean checkDirection(int row, int col, char symbol, int dRow, int dCol) {
-        int count = 0;
-
-        // forward
-        for (int i = 1; i < 5; i++) {
-            int r = row + i * dRow;
-            int c = col + i * dCol;
-            if (r >= 0 && r < 6 && c >= 0 && c < 6 && grid[r][c] == symbol) {
-                count++;
-            } else {
-                break;
-            }
-        }
-
-        // backward
-        for (int i = 1; i < 5; i++) {
-            int r = row - i * dRow;
-            int c = col - i * dCol;
-            if (r >= 0 && r < 6 && c >= 0 && c < 6 && grid[r][c] == symbol) {
-                count++;
-            } else {
-                break;
-            }
-        }
-
-        return count + 1 == 5;
-    }
 
     @Override
     public String toString() {
