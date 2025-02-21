b434e8851dda0ec2b74d40ff26ea37762c3bbf0e
thomas <deponte.thomas@gmail.com>
2025-02-21 22:47:04 +0100

    Use more simple approach first

--- app/src/main/java/org/example/Board.java
+++ app/src/main/java/org/example/Board.java
@@ -7,56 +7,7 @@ import java.util.List;
 public class Board {
     private List<Move> moves;
     private HashMap<Position, Move> positionMoves;
-
-    class movesInRow {
-        private List<Move> upDiagMoves;
-        private List<Move> DownDiagMoves;
-        private List<Move> horizMoves;
-        private List<Move> vertMoves;
-
-        public movesInRow() {
-            upDiagMoves = new ArrayList<>();
-            DownDiagMoves = new ArrayList<>();
-            horizMoves = new ArrayList<>();
-            vertMoves = new ArrayList<>();
-        }
-
-        public void addUpDiagMove(Move move) {
-            upDiagMoves.add(move);
-        }
-        public void addDownDiagMove(Move move) {
-            DownDiagMoves.add(move);
-        }
-        public void addHorizMove(Move move) {
-            horizMoves.add(move);
-        }
-        public void addVertMove(Move move) {
-            vertMoves.add(move);
-        }
-        public void shareUpDiagMoves(movesInRow other) {
-            if (other != null) {
-                other.upDiagMoves = this.upDiagMoves;
-            }
-        }
-        public void shareDownDiagMoves(movesInRow other) {
-            if (other != null) {
-                other.DownDiagMoves = this.DownDiagMoves;
-            }
-        }
-        public void shareHorizMoves(movesInRow other) {
-            if (other != null) {
-                other.horizMoves = this.horizMoves;
-            }
-        }
-        public void shareVertMoves(movesInRow other) {
-            if (other != null) {
-                other.vertMoves = this.vertMoves;
-            }
-        }
-    }
-
-    private HashMap<Position, movesInRow> adjMoves;
-
+    private boolean fiveInLineFound = false; // Internal variable to track if 5 in a line is found
 
     public Board() {
         this.moves = new ArrayList<>();
@@ -65,61 +16,71 @@ public class Board {
 
     public void addMove(Move move) {
         moves.add(move);
-        Position position = move.getMark().getPosition(); // TODO: use getter
-        if (!positionMoves.containsKey(position) && !adjMoves.containsKey(position)) {
+        Position position = move.getMark().getPosition(); // TODO: move getters
+        if (!positionMoves.containsKey(position)) {
             positionMoves.put(position, move);
 
-            // check adjancent moves
-            movesInRow adjMoveList = new movesInRow();
-
-            Position adjPosUpLeft;
-            Position adjPosDownRight;
-
-            boolean isUpLeft = false;
-            boolean isDownRight = false;
-
-           
-            if(!position.isOnLeftEdge() || !position.isOnTopEdge()) {
-                adjPosUpLeft = position.getUpLeft();
-                if (positionMoves.containsKey(adjPosUpLeft)) {
-                    if (positionMoves.get(adjPosUpLeft).isMarkTypeEqual(move)) {
-                        adjMoves.get(adjPosUpLeft).addDownDiagMove(move);
-                        adjMoveList.shareDownDiagMoves(adjMoves.get(adjPosUpLeft));
-                        isUpLeft = true;
-                    }
-                }
-            }
-            if(!position.isOnRightEdge() || !position.isOnBottomEdge()) {
-                adjPosDownRight = position.getDownRight();
-                if (positionMoves.containsKey(adjPosDownRight)) {
-                    if (positionMoves.get(adjPosDownRight).isMarkTypeEqual(move)) {
-                        adjMoves.get(adjPosDownRight).addDownDiagMove(move);
-                        adjMoveList.shareDownDiagMoves(adjMoves.get(adjPosDownRight));
-                        isDownRight = true;
-                    }
-                }
-            }
-
-            if (isUpLeft && isDownRight) {
-                
-            }
-
- 
+            checkAdjacentMoves(position, move);
         } else {
             throw new IllegalArgumentException("Multiple moves to same position are not allowed");
         }
+    }
 
+    private void checkAdjacentMoves(Position position, Move move) {
+        Position adjPosUpLeft = null;
+        Position adjPosDownRight = null;
+
+        if (!position.isOnLeftEdge() && !position.isOnTopEdge()) {
+            adjPosUpLeft = position.getUpLeft();
+        }
+        if (!position.isOnRightEdge() && !position.isOnBottomEdge()) {
+            adjPosDownRight = position.getDownRight();
+        }
+
+        int countUpLeft = 0;
+        int countDownRight = 0;
+
+        if (adjPosUpLeft != null) {
+            Move adjMoveUpLeft = positionMoves.get(adjPosUpLeft);
+            while (adjMoveUpLeft != null && adjMoveUpLeft.isMarkTypeEqual(move)) {
+                countUpLeft++;
+                Position nextPosition = adjMoveUpLeft.getMark().getPosition();
+                if (!nextPosition.isOnLeftEdge() && !nextPosition.isOnTopEdge()) {
+                    adjMoveUpLeft = positionMoves.get(nextPosition.getUpLeft());
+                } else {
+                    break;
+                }
+            }
+        }
+
+        if (adjPosDownRight != null) {
+            Move adjMoveDownRight = positionMoves.get(adjPosDownRight);
+            while (adjMoveDownRight != null && adjMoveDownRight.isMarkTypeEqual(move)) {
+                countDownRight++;
+                Position nextPosition = adjMoveDownRight.getMark().getPosition();
+                if (!nextPosition.isOnRightEdge() && !nextPosition.isOnBottomEdge()) {
+                    adjMoveDownRight = positionMoves.get(nextPosition.getDownRight());
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // Check if the total count of consecutive same-mark moves is exactly 5
+        if (countUpLeft + countDownRight == 4) { // 4 because we are counting from the current move position
+            fiveInLineFound = true;
+        }
     }
 
     public void clearBoard() {
         moves.clear();
         positionMoves.clear();
+        fiveInLineFound = false; // Reset the internal variable
     }
 
     public boolean isOccupied(Position position) {
         return positionMoves.containsKey(position);
     }
-
 
     @Override
     public String toString() {
@@ -136,5 +97,9 @@ public class Board {
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    public boolean isFiveInLineFound() {
+        return fiveInLineFound;
     }
 }
