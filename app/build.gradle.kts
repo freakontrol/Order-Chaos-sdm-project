8f46d44bd1696acff1187f5c43a143fab9cf7ef7
thomas <deponte.thomas@gmail.com>
2025-04-12 17:29:33 +0200

    Corrected local build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.Main"
+    mainClass = "orderandchaos.core.Main"
 }
 
 tasks.named<Test>("test") {
